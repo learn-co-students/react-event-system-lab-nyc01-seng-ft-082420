328793ab1e72a643be37549588cff8235eda48ac
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-27 17:00:54 -0400

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default function EyesOnMe(props) {
+    return (
+        <button onFocus={e=>console.log("Good!")} onBlur={e=>console.log('Hey! Eyes on me!')}>Button</button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,8 @@
 // Code Keypad Component Here
+import React from 'react';
+
+export default function Keypad(props) {
+    return (
+        <input onKeyUp={e => console.log("Entering password...")} type="password" />
+    )
+}
