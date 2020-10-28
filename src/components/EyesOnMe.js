2fb4d0c7c9f7b4f324e9b498c3d2bc77038eb23a
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-27 22:34:18 -0400

    made keypad

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+
+//   HandleKeyUp = (e) => {
+//     console.log('Entering password...')
+//   }
+  render () {
+    return (
+      <div></div>
+    )
+  }
+  }
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+  HandleKeyUp = (e) => {
+    console.log('Entering password...')
+  }
+  render () {
+    return (
+      <input onKeyUp={this.HandleKeyUp} type="password" />
+    )
+  }
+}
+export default Keypad
