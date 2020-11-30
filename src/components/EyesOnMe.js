3c5f39f3cd5b7748995215d604bb56074417eeea
D1yr <alexander.m.sahli@gmail.com>
2020-11-30 05:39:38 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+    blurMessage = () => {
+        console.log("Hey! Eyes on me!")
+    }
+    focusMessage = () => {
+        console.log("Good!")
+    }
+    render(){
+        return(
+            <div>
+                <button onFocus={this.focusMessage} onBlur={this.blurMessage}>Button</button>
+            </div>
+        )
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+    replyMessage = () => {
+        return console.log("Entering password...")
+    }
+    render(){
+        return(
+            <div>
+                <input onKeyUp={this.replyMessage} type="password" />
+            </div>
+        )
+    }
+}
+
+export default Keypad;
